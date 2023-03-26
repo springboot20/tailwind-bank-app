be9d0c1518aa3ae945d84f880cee16ae494925da
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-03-26 23:55:54 +0100

    modified

--- js/transaction.js
+++ js/transaction.js
@@ -1,92 +1,96 @@
 import showMenu, { addActive } from "./helper.js";
 
 (() => {
-	showMenu("show-icon", "navigation", "search-icon", "fa-bars");
+  showMenu("show-icon", "navigation", "search-icon", "fa-bars");
 })();
 
 addEventListener("load", () => {
-	addActive('active', 'a')
-	document.body.classList.add("loaded");
+  addActive("active", "a");
+  document.body.classList.add("loaded");
 });
 
 (() => {
-	let win = window;
-	let doc = win.document;
-	let ele = doc.documentElement;
-	let storage = localStorage;
+  let win = window;
+  let doc = win.document;
+  let ele = doc.documentElement;
+  let storage = localStorage;
 
-	let prefer_key = 'theme';
-	let pref = storage.getItem(prefer_key);
+  let prefer_key = "theme";
+  let pref = storage.getItem(prefer_key);
 
-	let dark = 'dark';
-	let light = 'light';
-	let toggle = doc.getElementById('checkbox');
-	let mode = storage.getItem('theme');
-	let modeText = doc.querySelector('.mode-txt');
-	let moonIcon = doc.querySelector('.moon');
-	let sunIcon = doc.querySelector('.sun');
+  let dark = "dark";
+  let light = "light";
+  let toggle = doc.getElementById("checkbox");
+  let mode = storage.getItem("theme");
+  let modeText = doc.querySelector(".mode-txt");
+  let moonIcon = doc.querySelector(".moon");
+  let sunIcon = doc.querySelector(".sun");
 
-	if (mode !== null && mode === dark) {
-		toggle.setAttribute('checked', true)
-		modeText.textContent = 'Dark mode'
+  if (mode !== null && mode === dark) {
+    toggle.setAttribute("checked", true);
+    modeText.textContent = "Dark mode";
 
-		moonIcon.style.opacity = '1'
-		sunIcon.style.opacity = '0'
-	}
+    moonIcon.style.opacity = "1";
+    sunIcon.style.opacity = "0";
+  }
 
-	let defaultTheme = light;
-	let active = (defaultTheme === dark);
+  let defaultTheme = light;
+  let active = defaultTheme === dark;
 
-	let activateTheme = (theme) => {
-		ele.classList.remove(dark, light);
-		ele.classList.add(theme)
+  let activateTheme = (theme) => {
+    ele.classList.remove(dark, light);
+    ele.classList.add(theme);
 
-		active = (theme === dark);
-	}
+    active = theme === dark;
+  };
 
-	if (pref === dark) activateTheme(dark);
-	if (pref === light) activateTheme(light);
+  if (pref === dark) activateTheme(dark);
+  if (pref === light) activateTheme(light);
 
-	if (!pref) {
-		let preferTheme = (theme) => {
-			return `(prefers-color-scheme: ${theme})`
-		}
+  if (!pref) {
+    let preferTheme = (theme) => {
+      return `(prefers-color-scheme: ${theme})`;
+    };
 
-		if (win.matchMedia(preferTheme(dark)).matches) {
-			activateTheme(dark)
-		} else if (win.matchMedia(preferTheme(light)).matches) {
-			activateTheme(light)
-		} else {
-			activateTheme(defaultTheme)
-		}
+    if (win.matchMedia(preferTheme(dark)).matches) {
+      activateTheme(dark);
+    } else if (win.matchMedia(preferTheme(light)).matches) {
+      activateTheme(light);
+    } else {
+      activateTheme(defaultTheme);
+    }
 
-		win.matchMedia(preferTheme(dark)).addEventListener("change", (event) => {
-			if (event.matches) activateTheme(dark);
-		})
-		win.matchMedia(preferTheme(light)).addEventListener("change", (event) => {
-			if (event.matches) activateTheme(light);
-		})
-	}
+    win.matchMedia(preferTheme(dark)).addEventListener("change", (event) => {
+      if (event.matches) activateTheme(dark);
+    });
+    win.matchMedia(preferTheme(light)).addEventListener("change", (event) => {
+      if (event.matches) activateTheme(light);
+    });
+  }
 
-	if (toggle) {
-		toggle.style.visibility = 'visible';
+  if (toggle) {
+    toggle.style.visibility = "visible";
 
-		toggle.addEventListener('change', () => {
-			if (active) {
-				activateTheme(light)
-				modeText.textContent = 'Light mode'
-				storage.setItem(prefer_key, light);
-				moonIcon.style.opacity = '0'
-				sunIcon.style.opacity = '1'
-			} else {
-				activateTheme(dark)
-				modeText.textContent = 'Dark mode'
-				storage.setItem(prefer_key, dark);
-				moonIcon.style.opacity = '1'
-				sunIcon.style.opacity = '0'
-			}
-		}, true)
-	}
+    toggle.addEventListener(
+      "change",
+      () => {
+        if (active) {
+          activateTheme(light);
+          modeText.textContent = "Light mode";
+          storage.setItem(prefer_key, light);
+          moonIcon.style.opacity = "0";
+          sunIcon.style.opacity = "1";
+        } else {
+          activateTheme(dark);
+          modeText.textContent = "Dark mode";
+          storage.setItem(prefer_key, dark);
+          moonIcon.style.opacity = "1";
+          sunIcon.style.opacity = "0";
+        }
+      },
+      true
+    );
+  }
 })();
 
 let AllUserAccounts = [];
@@ -94,25 +98,28 @@ let transactionHistory = [];
 let balance = 0;
 
 function getPreviousTransactionHistoryState() {
-	let ind = JSON.parse(localStorage.getItem("user-index"));
-	let currentUserTransactionHistory = JSON.parse(localStorage.getItem("users-accounts"))[ind]._transactionsHistory;
-	if (currentUserTransactionHistory) {
-		transactionHistory = currentUserTransactionHistory;
-	}
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  let currentUserTransactionHistory = JSON.parse(
+    localStorage.getItem("users-accounts")
+  )[ind]._transactionsHistory;
+  if (currentUserTransactionHistory) {
+    transactionHistory = currentUserTransactionHistory;
+  }
 }
 
 function getPreviousBalance() {
-	let ind = JSON.parse(localStorage.getItem("user-index"));
-	let currentUserBalance = JSON.parse(localStorage.getItem("users-accounts"))[ind]
-		._balance;
-	if (currentUserBalance) {
-		balance = currentUserBalance;
-	}
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  let currentUserBalance = JSON.parse(localStorage.getItem("users-accounts"))[
+    ind
+  ]._balance;
+  if (currentUserBalance) {
+    balance = currentUserBalance;
+  }
 }
 
 addEventListener("load", () => {
-	getPreviousTransactionHistoryState();
-	getPreviousBalance();
+  getPreviousTransactionHistoryState();
+  getPreviousBalance();
 });
 
 const select = document.querySelector("select#bank-name");
@@ -120,87 +127,84 @@ const accountnumber = document.getElementById("account-number");
 const accountname = document.getElementById("account-name");
 const amount = document.getElementById("amount");
 
-let error = document.querySelector('.error-notification-message');
-let success = document.querySelector('.notification-message');
+let error = document.querySelector(".error-notification-message");
+let success = document.querySelector(".notification-message");
 
 const transferForm = document.getElementById("transaction-form");
 
 const setTransactionHistory = () => {
-	let bankname = select.value;
-	console.log(bankname);
+  let bankname = select.value;
+  console.log(bankname);
 
-	let newTransactionsHistory = {
-		bankName: bankname,
-		accountName: accountname.value,
-		accountNumber: accountnumber.value,
-		amount: Number(amount.value),
-		time: new Date().toLocaleString()
-	};
+  let newTransactionsHistory = {
+    bankName: bankname,
+    accountName: accountname.value,
+    accountNumber: accountnumber.value,
+    amount: Number(amount.value),
+    time: new Date().toLocaleString(),
+  };
 
-	let ind = JSON.parse(localStorage.getItem("user-index"));
-	AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
 
-	transactionHistory.push(newTransactionsHistory);
-	AllUserAccounts[ind]._transactionsHistory = transactionHistory;
-	localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
+  transactionHistory.push(newTransactionsHistory);
+  AllUserAccounts[ind]._transactionsHistory = transactionHistory;
+  localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
 };
 
 const setBalance = () => {
-	let ind = JSON.parse(localStorage.getItem('user-index'));
-	AllUserAccounts = JSON.parse(localStorage.getItem('users-accounts'));
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
 
-	let prevBalance = JSON.parse(localStorage.getItem('users-accounts'))[ind]._balance;
+  let prevBalance = JSON.parse(localStorage.getItem("users-accounts"))[ind]
+    ._balance;
 
-	if (parseInt(prevBalance) < parseInt(Number(amount.value))) {
-		error.classList.add('active');
-	} else {
-		let newBalance = parseInt(prevBalance) - parseInt(Number(amount.value));
-		balance = newBalance;
-		AllUserAccounts[ind]._balance = balance;
-		console.log(newBalance, 'At line 86 in vscode');
+  if (parseInt(prevBalance) < parseInt(Number(amount.value))) {
+    alert("Insufficient balance");
+  } else {
+    let newBalance = parseInt(prevBalance) - parseInt(Number(amount.value));
+    balance = newBalance;
+    AllUserAccounts[ind]._balance = balance;
 
-		success.classList.add('active');
+    setTimeout(() => {
+      window.location.href = transferForm.getAttribute("action");
+    }, 5000);
+  }
 
-		setTimeout(() => {
-			window.location.href = transferForm.getAttribute('action');
-			success.classList.remove('active');
-		}, 5000)
-	}
+  localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
 
-	localStorage.setItem('users-accounts', JSON.stringify(AllUserAccounts));
-
-	console.log(prevBalance);
-	console.log(AllUserAccounts);
-}
-
+  console.log(prevBalance);
+  console.log(AllUserAccounts);
+};
 
 transferForm.addEventListener("submit", (event) => {
-	event.preventDefault();
+  event.preventDefault();
 
-	let fields = Array.from(document.querySelectorAll('.field'));
-	if (select.value === "" || accountnumber.value === "" || accountname.value === "" || amount.value === "") {
-		fields.forEach((field) => {
-			field.classList.add('error', 'shake');
-			field.classList.remove('valid');
-		})
-	} else {
-		fields.forEach((field) => {
-			field.classList.add('valid');
-			field.classList.remove('error', 'shake');
-		});
+  let fields = Array.from(document.querySelectorAll(".field"));
+  if (
+    select.value === "" ||
+    accountnumber.value === "" ||
+    accountname.value === "" ||
+    amount.value === ""
+  ) {
+    fields.forEach((field) => {
+      field.classList.add("error", "shake");
+      field.classList.remove("valid");
+    });
+  } else {
+    fields.forEach((field) => {
+      field.classList.add("valid");
+      field.classList.remove("error", "shake");
+    });
 
-		setTransactionHistory();
-		setBalance();
-		console.log(AllUserAccounts);
-	}
+    setTransactionHistory();
+    setBalance();
+    console.log(AllUserAccounts);
+  }
 
-	setTimeout(() => {
-		error.classList.remove('active')
-	}, 4500);
-
-	setTimeout(() => {
-		fields.forEach((field) => {
-			field.classList.remove('error', 'shake');
-		})
-	}, 2000)
+  setTimeout(() => {
+    fields.forEach((field) => {
+      field.classList.remove("error", "shake");
+    });
+  }, 2000);
 });
--- js/transactionIn.js
+++ js/transactionIn.js
@@ -1,91 +1,95 @@
 /**
  * FORM SCRIPT SCOPE
-*/
-import showMenu, { addActive } from "./helper.js"
+ */
+import showMenu, { addActive } from "./helper.js";
 
 (() => {
-	showMenu('show-icon', 'navigation', 'search-icon', 'fa-bars');
-	addActive('active', 'a');
+  showMenu("show-icon", "navigation", "search-icon", "fa-bars");
+  addActive("active", "a");
 })();
 
 (() => {
-	let win = window;
-	let doc = win.document;
-	let ele = doc.documentElement;
-	let storage = localStorage;
+  let win = window;
+  let doc = win.document;
+  let ele = doc.documentElement;
+  let storage = localStorage;
 
-	let prefer_key = 'theme';
-	let pref = storage.getItem(prefer_key);
+  let prefer_key = "theme";
+  let pref = storage.getItem(prefer_key);
 
-	let dark = 'dark';
-	let light = 'light';
-	let toggle = doc.getElementById('checkbox');
-	let mode = storage.getItem('theme');
-	let modeText = doc.querySelector('.mode-txt');
-	let moonIcon = doc.querySelector('.moon');
-	let sunIcon = doc.querySelector('.sun');
+  let dark = "dark";
+  let light = "light";
+  let toggle = doc.getElementById("checkbox");
+  let mode = storage.getItem("theme");
+  let modeText = doc.querySelector(".mode-txt");
+  let moonIcon = doc.querySelector(".moon");
+  let sunIcon = doc.querySelector(".sun");
 
-	if (mode !== null && mode === dark) {
-		toggle.setAttribute('checked', true)
-		modeText.textContent = 'Dark mode';
+  if (mode !== null && mode === dark) {
+    toggle.setAttribute("checked", true);
+    modeText.textContent = "Dark mode";
 
-		moonIcon.style.opacity = '1'
-		sunIcon.style.opacity = '0'
-	}
+    moonIcon.style.opacity = "1";
+    sunIcon.style.opacity = "0";
+  }
 
-	let defaultTheme = light;
-	let active = (defaultTheme === dark);
+  let defaultTheme = light;
+  let active = defaultTheme === dark;
 
-	let activateTheme = (theme) => {
-		ele.classList.remove(dark, light);
-		ele.classList.add(theme)
+  let activateTheme = (theme) => {
+    ele.classList.remove(dark, light);
+    ele.classList.add(theme);
 
-		active = (theme === dark);
-	}
+    active = theme === dark;
+  };
 
-	if (pref === dark) activateTheme(dark);
-	if (pref === light) activateTheme(light);
+  if (pref === dark) activateTheme(dark);
+  if (pref === light) activateTheme(light);
 
-	if (!pref) {
-		let preferTheme = (theme) => {
-			return `(prefers-color-scheme: ${theme})`
-		}
+  if (!pref) {
+    let preferTheme = (theme) => {
+      return `(prefers-color-scheme: ${theme})`;
+    };
 
-		if (win.matchMedia(preferTheme(dark)).matches) {
-			activateTheme(dark)
-		} else if (win.matchMedia(preferTheme(light)).matches) {
-			activateTheme(light)
-		} else {
-			activateTheme(defaultTheme)
-		}
+    if (win.matchMedia(preferTheme(dark)).matches) {
+      activateTheme(dark);
+    } else if (win.matchMedia(preferTheme(light)).matches) {
+      activateTheme(light);
+    } else {
+      activateTheme(defaultTheme);
+    }
 
-		win.matchMedia(preferTheme(dark)).addEventListener("change", (event) => {
-			if (event.matches) activateTheme(dark);
-		})
-		win.matchMedia(preferTheme(light)).addEventListener("change", (event) => {
-			if (event.matches) activateTheme(light);
-		})
-	}
+    win.matchMedia(preferTheme(dark)).addEventListener("change", (event) => {
+      if (event.matches) activateTheme(dark);
+    });
+    win.matchMedia(preferTheme(light)).addEventListener("change", (event) => {
+      if (event.matches) activateTheme(light);
+    });
+  }
 
-	if (toggle) {
-		toggle.style.visibility = 'visible';
+  if (toggle) {
+    toggle.style.visibility = "visible";
 
-		toggle.addEventListener('change', () => {
-			if (active) {
-				activateTheme(light)
-				modeText.textContent = 'Light mode'
-				storage.setItem(prefer_key, light);
-				moonIcon.style.opacity = '0'
-				sunIcon.style.opacity = '1'
-			} else {
-				activateTheme(dark)
-				modeText.textContent = 'Dark mode'
-				storage.setItem(prefer_key, dark);
-				moonIcon.style.opacity = '1'
-				sunIcon.style.opacity = '0'
-			}
-		}, true)
-	}
+    toggle.addEventListener(
+      "change",
+      () => {
+        if (active) {
+          activateTheme(light);
+          modeText.textContent = "Light mode";
+          storage.setItem(prefer_key, light);
+          moonIcon.style.opacity = "0";
+          sunIcon.style.opacity = "1";
+        } else {
+          activateTheme(dark);
+          modeText.textContent = "Dark mode";
+          storage.setItem(prefer_key, dark);
+          moonIcon.style.opacity = "1";
+          sunIcon.style.opacity = "0";
+        }
+      },
+      true
+    );
+  }
 })();
 
 let AllUserAccounts = [];
@@ -93,94 +97,98 @@ let transactionsInHistory = [];
 let balance = 0;
 
 function getPreviousTransactionInHistory() {
-	let ind = JSON.parse(localStorage.getItem("user-index"));
-	let currentUserTransactionsInHistory = JSON.parse(localStorage.getItem("users-accounts"))[ind]._transactionsInHistory;
-	if (currentUserTransactionsInHistory) {
-		transactionsInHistory = currentUserTransactionsInHistory;
-	}
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  let currentUserTransactionsInHistory = JSON.parse(
+    localStorage.getItem("users-accounts")
+  )[ind]._transactionsInHistory;
+  if (currentUserTransactionsInHistory) {
+    transactionsInHistory = currentUserTransactionsInHistory;
+  }
 }
 
 function getPreviousBalance() {
-	let ind = JSON.parse(localStorage.getItem("user-index"));
-	let currentUserBalance = JSON.parse(localStorage.getItem("users-accounts"))[ind]
-		._balance;
-	if (currentUserBalance) {
-		balance = currentUserBalance;
-	}
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  let currentUserBalance = JSON.parse(localStorage.getItem("users-accounts"))[
+    ind
+  ]._balance;
+  if (currentUserBalance) {
+    balance = currentUserBalance;
+  }
 }
 
-let sourcename = document.querySelector('select#source-name');
-let amount = document.getElementById('amount');
-let msg = document.getElementById('message');
+let sourcename = document.querySelector("select#source-name");
+let amount = document.getElementById("amount");
+let msg = document.getElementById("message");
 
 const setTransactionsInHistory = () => {
-	let ind = JSON.parse(localStorage.getItem('user-index'));
-	AllUserAccounts = JSON.parse(localStorage.getItem('users-accounts'));
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
 
-	let newTransactionsInHistory = {
-		_id: new Date().getTime().toString(36) + new Date().getUTCMilliseconds(),
-		time: new Date().toLocaleString(),
-		sourceName: sourcename.value,
-		message: msg.value,
-		amount: Number(amount.value)
-	};
+  let newTransactionsInHistory = {
+    _id: new Date().getTime().toString(36) + new Date().getUTCMilliseconds(),
+    time: new Date().toLocaleString(),
+    sourceName: sourcename.value,
+    message: msg.value,
+    amount: Number(amount.value),
+  };
 
-	transactionsInHistory.push(newTransactionsInHistory);
-	AllUserAccounts[ind]._transactionsInHistory = transactionsInHistory
-	localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
-}
+  transactionsInHistory.push(newTransactionsInHistory);
+  AllUserAccounts[ind]._transactionsInHistory = transactionsInHistory;
+  localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
+};
 
 const setBalance = () => {
-	let ind = JSON.parse(localStorage.getItem('user-index'));
-	AllUserAccounts = JSON.parse(localStorage.getItem('users-accounts'));
+  let ind = JSON.parse(localStorage.getItem("user-index"));
+  AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
 
-	let transactionAmount = JSON.parse(localStorage.getItem('users-accounts'))[ind]._balance;
-	let newBalance = parseInt(transactionAmount) + parseInt(Number(amount.value))
+  let transactionAmount = JSON.parse(localStorage.getItem("users-accounts"))[
+    ind
+  ]._balance;
+  let newBalance = parseInt(transactionAmount) + parseInt(Number(amount.value));
 
-	balance = newBalance;
-	AllUserAccounts[ind]._balance += balance;
-	localStorage.setItem('users-accounts', JSON.stringify(AllUserAccounts));
+  balance = newBalance;
+  AllUserAccounts[ind]._balance += balance;
+  localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
 
-	console.log(transactionAmount);
-	console.log(AllUserAccounts);
-	console.log(newBalance, 'At line 89 in vscode');
-}
+  console.log(transactionAmount);
+  console.log(AllUserAccounts);
+  console.log(newBalance, "At line 89 in vscode");
+};
 
-let success = document.querySelector('.notification-message');
-document.querySelector('form').addEventListener('submit', (event) => {
-	event.preventDefault();
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-	let fields = Array.from(document.querySelectorAll('.field'));
-	if (sourcename.value.trim() === "" || amount.value.trim() === "") {
-		fields.forEach((field) => {
-			field.classList.add('error', 'shake');
-			field.classList.remove('valid');
-		})
-	} else {
-		fields.forEach((field) => {
-			field.classList.add('valid');
-			field.classList.remove('error', 'shake');
-		});
+  let fields = Array.from(document.querySelectorAll(".field"));
+  if (sourcename.value.trim() === "" || amount.value.trim() === "") {
+    fields.forEach((field) => {
+      field.classList.add("error", "shake");
+      field.classList.remove("valid");
+    });
+  } else {
+    fields.forEach((field) => {
+      field.classList.add("valid");
+      field.classList.remove("error", "shake");
+    });
 
-		setTransactionsInHistory();
-		setBalance();
-		success.classList.add('active')
+    setTransactionsInHistory();
+    setBalance();
 
-		setTimeout(() => {
-			window.location.href = document.querySelector('form').getAttribute('action');
-			success.classList.remove('active');
-		}, 4500)
-	}
+    setTimeout(() => {
+      window.location.href = document
+        .querySelector("form")
+        .getAttribute("action");
+    }, 4500);
+  }
 
-	setTimeout(() => {
-		fields.forEach((field) => {
-			field.classList.remove('error', 'shake');
-		})
-	}, 3000)
-})
+  setTimeout(() => {
+    fields.forEach((field) => {
+      field.classList.remove("error", "shake");
+    });
+  }, 3000);
+});
 
-window.addEventListener('load', () => {
-	document.body.classList.add('loaded');
-	getPreviousBalance();
-	getPreviousTransactionInHistory();
-})
+window.addEventListener("load", () => {
+  document.body.classList.add("loaded");
+  getPreviousBalance();
+  getPreviousTransactionInHistory();
+});
