e92baf199827e9defbd88c9f435469dea3ad72dc
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-03-26 19:24:34 +0100

    modified

--- js/sign.js
+++ js/sign.js
@@ -1,17 +1,14 @@
-
-
 let AllUserAccounts = [];
 
 function getUserPrevAccount() {
-	if (localStorage.getItem("users-accounts")) {
-		AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
-	}
+  if (localStorage.getItem("users-accounts")) {
+    AllUserAccounts = JSON.parse(localStorage.getItem("users-accounts"));
+  }
 }
 
 addEventListener("load", () => {
-	getUserPrevAccount();
+  getUserPrevAccount();
 });
-
 
 const form = document.querySelector("form");
 const firstname = document.getElementById("firstname");
@@ -28,83 +25,84 @@ const pField = document.querySelector(".password");
 const cField = document.querySelector(".confirm-password");
 
 const signUp = (event) => {
-	event.preventDefault();
+  event.preventDefault();
 
-	firstname.value == ""
-		? fField.classList.add("shake", "error")
-		: checkUsernameHandler(firstname, fField);
-	lastname.value == ""
-		? lField.classList.add("shake", "error")
-		: checkUsernameHandler(lastname, lField);
-	email.value == ""
-		? eField.classList.add("shake", "error")
-		: checkEmailHandler(email, eField);
-	passWord.value == ""
-		? pField.classList.add("shake", "error")
-		: checkPasswordHandler(passWord, pField);
-	confirmPassword.value == ""
-		? cField.classList.add("shake", "error")
-		: confirmHandler(confirmPassword, passWord, cField);
+  firstname.value == ""
+    ? fField.classList.add("shake", "error")
+    : checkUsernameHandler(firstname, fField);
+  lastname.value == ""
+    ? lField.classList.add("shake", "error")
+    : checkUsernameHandler(lastname, lField);
+  email.value == ""
+    ? eField.classList.add("shake", "error")
+    : checkEmailHandler(email, eField);
+  passWord.value == ""
+    ? pField.classList.add("shake", "error")
+    : checkPasswordHandler(passWord, pField);
+  confirmPassword.value == ""
+    ? cField.classList.add("shake", "error")
+    : confirmHandler(confirmPassword, passWord, cField);
 
-	let newUserAccount = {
-		firstName: firstname.value,
-		lastName: lastname.value,
-		emailAddress: email.value,
-		password: passWord.value,
-		confirmPass: confirmPassword.value,
-		_transactionsInHistory: [],
-		_transactionsHistory: [],
-		_balance: 0,
-		time: new Date().toLocaleString()
-	};
+  let newUserAccount = {
+    firstName: firstname.value,
+    lastName: lastname.value,
+    emailAddress: email.value,
+    password: passWord.value,
+    confirmPass: confirmPassword.value,
+    _transactionsInHistory: [],
+    _transactionsHistory: [],
+    _balance: 0,
+    time: new Date().toLocaleString(),
+  };
 
-	setTimeout(() => {
-		fField.classList.remove("shake", "error");
-		lField.classList.remove("shake", "error");
-		eField.classList.remove("shake", "error");
-		pField.classList.remove("shake", "error");
-		cField.classList.remove("shake", "error");
-	}, 1500);
+  setTimeout(() => {
+    fField.classList.remove("shake", "error");
+    lField.classList.remove("shake", "error");
+    eField.classList.remove("shake", "error");
+    pField.classList.remove("shake", "error");
+    cField.classList.remove("shake", "error");
+  }, 1500);
 
-	if (
-		!fField.classList.contains("error") &&
-		!lField.classList.contains("error") &&
-		!eField.classList.contains("error") &&
-		!pField.classList.contains("error") &&
-		!cField.classList.contains("error")
-	) {
-		AllUserAccounts.push(newUserAccount);
-		location.href = form.getAttribute("action");
+  if (
+    !fField.classList.contains("error") &&
+    !lField.classList.contains("error") &&
+    !eField.classList.contains("error") &&
+    !pField.classList.contains("error") &&
+    !cField.classList.contains("error")
+  ) {
+    AllUserAccounts.push(newUserAccount);
+    location.href = form.getAttribute("action");
 
-		const span = document.createElement("span");
-		span.className = "loader";
-		signButton.textContent = `Signing Up..........`;
-		signButton.disabled = true;
-		signButton.append(span);
-		localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
-	}
+    const span = document.createElement("span");
+    span.className = "loader";
+    signButton.textContent = `Signing Up..........`;
+    signButton.disabled = true;
+    signButton.append(span);
+    localStorage.setItem("users-accounts", JSON.stringify(AllUserAccounts));
+  }
 
-	console.log(AllUserAccounts);
+  console.log(AllUserAccounts);
 
-	firstname.addEventListener("keyup", () => {
-		checkUsernameHandler(firstname, fField);
-	});
-	lastname.addEventListener("keyup", () => {
-		checkUsernameHandler(lastname, lField);
-	});
-	email.addEventListener("keyup", () => {
-		checkEmailHandler(email, eField);
-	});
-	passWord.addEventListener("keyup", () => {
-		checkPasswordHandler(passWord, pField);
-	});
-	confirmPassword.addEventListener("keyup", () => {
-		confirmHandler(confirmPassword, passWord, cField);
-	});
+  firstname.addEventListener("keyup", () => {
+    checkUsernameHandler(firstname, fField);
+  });
+  lastname.addEventListener("keyup", () => {
+    checkUsernameHandler(lastname, lField);
+  });
+  email.addEventListener("keyup", () => {
+    checkEmailHandler(email, eField);
+  });
+  passWord.addEventListener("keyup", () => {
+    checkPasswordHandler(passWord, pField);
+  });
+  confirmPassword.addEventListener("keyup", () => {
+    confirmHandler(confirmPassword, passWord, cField);
+  });
 };
 
-
-form.addEventListener("submit", (event) => { signUp(event) });
+form.addEventListener("submit", (event) => {
+  signUp(event);
+});
 
 /**
  *
@@ -112,18 +110,18 @@ form.addEventListener("submit", (event) => { signUp(event) });
  * @param {*} userField
  */
 const checkUsernameHandler = (usernameInput, userField) => {
-	if (usernameInput.value.length < 6) {
-		userField.classList.add("error");
-		userField.classList.remove("valid");
+  if (usernameInput.value.length < 6) {
+    userField.classList.add("error");
+    userField.classList.remove("valid");
 
-		let errorTxt = userField.querySelector(".error-txt");
-		usernameInput.value != ""
-			? (errorTxt.textContent = "name input should not less than six")
-			: (errorTxt.textContent = "name input cannot be blanked");
-	} else {
-		userField.classList.remove("error");
-		userField.classList.add("valid");
-	}
+    let errorTxt = userField.querySelector(".error-txt");
+    usernameInput.value != ""
+      ? (errorTxt.textContent = "name input should not less than six")
+      : (errorTxt.textContent = "name input cannot be blanked");
+  } else {
+    userField.classList.remove("error");
+    userField.classList.add("valid");
+  }
 };
 
 /**
@@ -132,19 +130,19 @@ const checkUsernameHandler = (usernameInput, userField) => {
  * @param {*} emailField
  */
 const checkEmailHandler = (emailInput, emailField) => {
-	let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-	if (!emailInput.value.match(pattern)) {
-		emailField.classList.add("error");
-		emailField.classList.remove("valid");
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(pattern)) {
+    emailField.classList.add("error");
+    emailField.classList.remove("valid");
 
-		let errorTxt = emailField.querySelector(".error-txt");
-		email.value != ""
-			? (errorTxt.textContent = "Enter a valid email address")
-			: (errorTxt.textContent = "Email cannot be blanked");
-	} else {
-		emailField.classList.remove("error");
-		emailField.classList.add("valid");
-	}
+    let errorTxt = emailField.querySelector(".error-txt");
+    email.value != ""
+      ? (errorTxt.textContent = "Enter a valid email address")
+      : (errorTxt.textContent = "Email cannot be blanked");
+  } else {
+    emailField.classList.remove("error");
+    emailField.classList.add("valid");
+  }
 };
 
 /**
@@ -153,21 +151,21 @@ const checkEmailHandler = (emailInput, emailField) => {
  * @param {*} passField
  */
 const checkPasswordHandler = (passwordInput, passField) => {
-	let pattern =
-		/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[-\_\@]+)(?=.*[a-zA-Z]).{8,16}$/;
-	if (!passwordInput.value.match(pattern)) {
-		passField.classList.add("error");
-		passField.classList.remove("valid");
+  let pattern =
+    /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[-\_\@]+)(?=.*[a-zA-Z]).{8,16}$/;
+  if (!passwordInput.value.match(pattern)) {
+    passField.classList.add("error");
+    passField.classList.remove("valid");
 
-		let errorTxt = passField.querySelector(".error-txt");
-		passwordInput.value != ""
-			? (errorTxt.textContent =
-				"Password can only contains digits, special characters e.g (-,_,@) ,lower and upper case and should not less than 8")
-			: (errorTxt.textContent = "Password cannot be blanked");
-	} else {
-		passField.classList.remove("error");
-		passField.classList.add("valid");
-	}
+    let errorTxt = passField.querySelector(".error-txt");
+    passwordInput.value != ""
+      ? (errorTxt.textContent =
+          "Password can only contains digits, special characters e.g (-,_,@) ,lower and upper case and should not less than 8")
+      : (errorTxt.textContent = "Password cannot be blanked");
+  } else {
+    passField.classList.remove("error");
+    passField.classList.add("valid");
+  }
 };
 
 /**
@@ -177,17 +175,17 @@ const checkPasswordHandler = (passwordInput, passField) => {
  * @param {*} conField
  */
 const confirmHandler = (confirmInput, passwordInput, conField) => {
-	if (confirmInput.value !== passwordInput.value) {
-		conField.classList.add("error");
-		conField.classList.remove("valid");
+  if (confirmInput.value !== passwordInput.value) {
+    conField.classList.add("error");
+    conField.classList.remove("valid");
 
-		let errorTxt = conField.querySelector(".error-txt");
-		confirmInput.value !== passwordInput.value
-			? (errorTxt.textContent =
-				"Confirm password must be the same with password")
-			: (errorTxt.textContent = "Confirm password cannot be blanked");
-	} else {
-		conField.classList.remove("error");
-		conField.classList.add("valid");
-	}
+    let errorTxt = conField.querySelector(".error-txt");
+    confirmInput.value !== passwordInput.value
+      ? (errorTxt.textContent =
+          "Confirm password must be the same with password")
+      : (errorTxt.textContent = "Confirm password cannot be blanked");
+  } else {
+    conField.classList.remove("error");
+    conField.classList.add("valid");
+  }
 };
